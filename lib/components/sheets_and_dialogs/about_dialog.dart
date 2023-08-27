4be23fe37c95599bed60f5f1bc378d53d97d8b4c
cryndry <ismailkaraagac94@gmail.com>
2023-08-27 19:24:22 +0300

    About dialog modified

--- lib/components/sheets_and_dialogs/about_dialog.dart
+++ lib/components/sheets_and_dialogs/about_dialog.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:kelime_hazinem/components/sheets_and_dialogs/dialog.dart';
 import 'package:kelime_hazinem/utils/const_objects.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 Future<void> popAboutDialog(BuildContext context) async {
   final packageInfo = await PackageInfo.fromPlatform();
+
   popDialog(
     context: context,
     routeName: "AboutDialog",
     builder: (setDialogState) {
       return [
         Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ClipRRect(
               borderRadius: BorderRadius.circular(12),
@@ -33,18 +36,37 @@ Future<void> popAboutDialog(BuildContext context) async {
           ],
         ),
         const SizedBox(height: 16),
-        Text(
-          "${" " * 8}Uygulamamızın amacı Arapça öğrenen insanlara kelime ezberleme aşamasında kolay ulaşılabilen ve pratik bir yolla yardımcı olmaktır.",
-          style: MyTextStyles.font_16_24_500.apply(color: Colors.black87),
-        ),
-        const SizedBox(height: 8),
-        Text(
-          "İletişim: kelimehazinemm@gmail.com",
-          style: MyTextStyles.font_16_24_500.apply(color: Colors.black87),
+        Align(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints.tightFor(width: 280),
+            child: Text(
+              "Uygulamamızın amacı Arapça öğrenen insanlara kelime ezberleme aşamasında kolay ulaşılabilen ve pratik bir yolla yardımcı olmaktır.",
+              style: MyTextStyles.font_16_24_500.apply(color: Colors.black87),
+              textAlign: TextAlign.center,
+            ),
+          ),
         ),
         const SizedBox(height: 24),
+        Text(
+          "İletişim",
+          textAlign: TextAlign.center,
+          style: MyTextStyles.font_20_24_500.apply(color: Colors.black87),
+        ),
+        TextButton(
+          onPressed: () async {
+            final uri = Uri(
+              scheme: "mailto",
+              path: "kelimehazinemm@gmail.com",
+            );
+            launchUrl(uri);
+          },
+          child: const Text(
+            "kelimehazinemm@gmail.com",
+            style: MyTextStyles.font_16_20_500,
+          ),
+        ),
         Row(
-          mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             TextButton(
               child: const Text("Lisanslar"),
@@ -52,11 +74,12 @@ Future<void> popAboutDialog(BuildContext context) async {
                 showLicensePage(context: context);
               },
             ),
-            const SizedBox(width: 12),
             TextButton(
-              child: const Text("Geri Dön"),
-              onPressed: () {
-                Navigator.of(context).pop();
+              child: const Text("Gizlilik Politikası"),
+              onPressed: () async {
+                const url = "https://kelimehazinem.com/privacy-policy";
+                final uri = Uri.parse(url);
+                if (await canLaunchUrl(uri)) launchUrl(uri);
               },
             ),
           ],
