702247493ed7ccff2421113ec3c890cff5596b58
cryndry <ismailkaraagac94@gmail.com>
2023-08-30 13:05:55 +0300

    Word deletion undoing in word selection appbar now

--- lib/components/app_bars/word_selection_app_bar.dart
+++ lib/components/app_bars/word_selection_app_bar.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kelime_hazinem/components/app_bars/app_bar.dart';
 import 'package:kelime_hazinem/components/sheets_and_dialogs/add_word_to_lists.dart';
 import 'package:kelime_hazinem/components/sheets_and_dialogs/add_words_to_lists.dart';
 import 'package:kelime_hazinem/components/buttons/icon.dart';
+import 'package:kelime_hazinem/components/sheets_and_dialogs/undo_snack_bar.dart';
 import 'package:kelime_hazinem/main.dart';
 import 'package:kelime_hazinem/utils/analytics.dart';
 import 'package:kelime_hazinem/utils/const_objects.dart';
@@ -10,6 +12,7 @@ import 'package:kelime_hazinem/utils/database.dart';
 import 'package:kelime_hazinem/utils/my_svgs.dart';
 import 'package:kelime_hazinem/utils/navigation_observer.dart';
 import 'package:kelime_hazinem/utils/providers.dart';
+import 'package:kelime_hazinem/utils/word_db_model.dart';
 
 class WordSelectionAppBar extends ConsumerStatefulWidget {
   const WordSelectionAppBar({super.key});
@@ -23,6 +26,103 @@ class WordSelectionAppBarState extends ConsumerState<WordSelectionAppBar> {
     final isUsedInListCreateMode = MyNavigatorObserver.stack.first == "AddWordsToNewList";
     if (isUsedInListCreateMode) Navigator.of(context).pop();
     deactivateWordSelectionMode(ref);
+  }
+
+  void deleteWords(List<int> selectedWordIds) {
+    final isUsedInAllWordsOfListPage = MyNavigatorObserver.stack.first == "AllWordsOfList";
+
+    List<Word> Function(List<Word>) stateUpdater(
+      Map<int, Word> willRemoveIndexes,
+      List<MapEntry<int, Word>> willRemoveIndexesSorted,
+    ) {
+      return (state) {
+        final newState = [...state];
+
+        for (int index = 0; index < newState.length; index++) {
+          final Word word = newState[index];
+          if (selectedWordIds.contains(word.id)) {
+            willRemoveIndexes[index] = word;
+          }
+
+          if (selectedWordIds.length == willRemoveIndexes.length) break;
+        }
+
+        willRemoveIndexesSorted.addAll(
+          willRemoveIndexes.entries.toList()
+            ..sort(
+              (entry1, entry2) => entry2.key.compareTo(entry1.key),
+            ),
+        );
+
+        for (MapEntry<int, Word> entry in willRemoveIndexesSorted) {
+          newState.removeAt(entry.key);
+        }
+
+        return newState;
+      };
+    }
+
+    final willRemoveIndexesAllWords = <int, Word>{};
+    List<MapEntry<int, Word>> willRemoveIndexesSortedAllWords = [];
+    ref.read(allWordsProvider.notifier).update(stateUpdater(
+          willRemoveIndexesAllWords,
+          willRemoveIndexesSortedAllWords,
+        ));
+
+    final willRemoveIndexesAllWordsOfList = <int, Word>{};
+    List<MapEntry<int, Word>> willRemoveIndexesSortedAllWordsOfList = [];
+    List<Word> remainedWordsInAllWordsOfListPage = [];
+    if (isUsedInAllWordsOfListPage) {
+      remainedWordsInAllWordsOfListPage = ref.read(allWordsOfListProvider.notifier).update(stateUpdater(
+            willRemoveIndexesAllWordsOfList,
+            willRemoveIndexesSortedAllWordsOfList,
+          ));
+    }
+
+    deactivateWordSelectionMode(ref);
+    // appBarRef is used, because ref of WordSelectionAppBarState is not accessible after above line executed.
+    // because ref is actually context of Consumer(Stateful)Widget
+    final appBarRef = context.findAncestorStateOfType<MyAppBarState>()!.ref;
+
+    showUndoSnackBar(
+      message: "${selectedWordIds.length == 1 ? "Kelime" : "Kelimeler"} kalıcı olarak silinecek.",
+      duration: MyDurations.millisecond1000 * 5,
+      undoCallback: () {
+        appBarRef.read(allWordsProvider.notifier).update((state) {
+          final newState = [...state];
+
+          for (MapEntry<int, Word> entry in willRemoveIndexesSortedAllWords.reversed) {
+            newState.insert(entry.key, entry.value);
+          }
+
+          return newState;
+        });
+
+        if (isUsedInAllWordsOfListPage) {
+          appBarRef.read(allWordsOfListProvider.notifier).update((state) {
+            final newState = [...state];
+
+            for (MapEntry<int, Word> entry in willRemoveIndexesSortedAllWordsOfList.reversed) {
+              newState.insert(entry.key, entry.value);
+            }
+
+            return newState;
+          });
+        }
+      },
+      noUndoCallback: () {
+        for (MapEntry<int, Word> entry in willRemoveIndexesSortedAllWords) {
+          final Word word = entry.value;
+          SqlDatabase.deleteWord(word.id);
+          Analytics.logWordAction(word: word.word, action: "word_deleted");
+        }
+
+        final isAllWordsOfListEmpty = remainedWordsInAllWordsOfListPage.isEmpty;
+        if (MyNavigatorObserver.stack.first == "AllWordsOfList" && isAllWordsOfListEmpty) {
+          Navigator.of(KelimeHazinem.navigatorKey.currentContext!).popUntil(ModalRoute.withName("MainScreen"));
+        }
+      },
+    );
   }
 
   @override
@@ -68,41 +168,7 @@ class WordSelectionAppBarState extends ConsumerState<WordSelectionAppBar> {
                 size: 32,
                 semanticsLabel: "${selectedWords.length == 1 ? "Kelimeyi" : "Kelimeleri"} Sil",
                 onTap: () {
-                  ref.read(allWordsProvider.notifier).update((state) {
-                    final newState = [...state];
-                    newState.removeWhere((word) {
-                      final bool result = selectedWords.contains(word.id);
-                      if (result) {
-                        SqlDatabase.deleteWord(word.id);
-                        Analytics.logWordAction(word: word.word, action: "word_deleted");
-                      }
-                      return result;
-                    });
-
-                    return newState;
-                  });
-
-                  bool isAllWordsOfListEmpty = false;
-                  ref.read(allWordsOfListProvider.notifier).update((state) {
-                    if (state.isEmpty) {
-                      isAllWordsOfListEmpty = true;
-                      return state;
-                    }
-
-                    final newState = [...state];
-                    newState.removeWhere((word) => selectedWords.contains(word.id));
-
-                    if (newState.isEmpty) isAllWordsOfListEmpty = true;
-
-                    return newState;
-                  });
-
-                  deactivateWordSelectionMode(ref);
-
-                  if (MyNavigatorObserver.stack.first == "AllWordsOfList" && isAllWordsOfListEmpty) {
-                    Navigator.of(KelimeHazinem.navigatorKey.currentContext!)
-                        .popUntil(ModalRoute.withName("MainScreen"));
-                  }
+                  deleteWords([...selectedWords]);
                 },
               ),
             ),
